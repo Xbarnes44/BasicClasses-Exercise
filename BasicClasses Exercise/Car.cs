12c3f74f8c115b695fdc1ab2fa23c3bdbcf0a2b6
Xavier Barnes <xdbcoder44@gmail.com>
2022-09-05 15:03:53 -0400

    Add project files.

---
+++ BasicClasses Exercise/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClasses_Exercise
+{
+    public class Car
+    {
+
+        public Car(string make, string model, int year)
+        {
+
+        }
+        public Car()
+        {
+
+        }
+
+        // Create a MAKE property of type string that is public 
+        public string Make { get; set; } // get= read, set = write
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }   
+    }
+}
---
+++ BasicClasses Exercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using BasicClasses_Exercise;
+using System;
+using System.Collections.Generic;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car(); // Constuctor - special member method. Helps us create a new instance of the Car class
+            myCar.Make = "Ford";
+            myCar.Model = "Focus";
+            myCar.Year = 2008;
+
+            var mazda = new Car()
+            {
+                Make = "Mazda" ,
+                Model = "Cx-5" ,
+                Year = 2016
+            };
+
+            var chevy = new Car("Chevy", "Impala", 2001)
+            {
+
+            };
+
+        }
+    }
+}
